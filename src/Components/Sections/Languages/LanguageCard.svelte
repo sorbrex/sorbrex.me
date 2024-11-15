0a975b38161c0ea20c33bae222a7a472a47b8502
Nando <nsorbello@movia.biz>
2024-11-15 11:05:18 +0100

    remove static link for icons and use iconify

--- src/Components/Sections/Languages/LanguageCard.svelte
+++ src/Components/Sections/Languages/LanguageCard.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
+	import Icon from '@iconify/svelte';
+
 	export let name: string;
 	export let source: string;
-	export let alt: string;
 	export let color: string;
 </script>
 
 <div class="flex flex-col items-center justify-center m-2 min-w-16">
-	<img src={`https://cdn.simpleicons.org/${source}/${color}`} {alt} class="h-16 w-16 shadow mb-1" />
+	<Icon icon={source} {color} class="h-16 w-16 shadow mb-1" />
 	<h2 class="text-subtitleColor font-bold text-lg mb-3">{name}</h2>
 </div>
--- src/Components/Sections/Languages/Languages.svelte
+++ src/Components/Sections/Languages/Languages.svelte
@@ -6,38 +6,53 @@
 	<h1 class="text-3xl font-bold text-titleColor">Languages</h1>
 
 	<div class="flex flex-row items-center justify-around mt-5 flex-wrap">
-		<LanguageCard source="typescript" color="#3178C6" alt="typescript" name="TypeScript" />
+		<LanguageCard
+			source="simple-icons:typescript"
+			color="#3178C6"
+			alt="typescript"
+			name="TypeScript"
+		/>
 
-		<LanguageCard source="react" color="#61DAFB" alt="react" name="React" />
+		<LanguageCard source="simple-icons:react" color="#61DAFB" alt="react" name="React" />
 
-		<LanguageCard source="svelte" color="#FF3E00" alt="svelte" name="SvelteKit" />
+		<LanguageCard source="simple-icons:svelte" color="#FF3E00" alt="svelte" name="SvelteKit" />
 
-		<LanguageCard source="tailwindcss" color="#06B6D4" alt="tailwindcss" name="Tailwind" />
+		<LanguageCard
+			source="simple-icons:tailwindcss"
+			color="#06B6D4"
+			alt="tailwindcss"
+			name="Tailwind"
+		/>
 
-		<LanguageCard source="swift" color="#F05138" alt="swift" name="Swift" />
+		<LanguageCard source="simple-icons:swift" color="#F05138" alt="swift" name="Swift" />
 	</div>
 
 	<div class="flex flex-row items-center justify-around mt-5 flex-wrap">
-		<LanguageCard source="nodedotjs" color="#339933" alt="nodedotjs" name="NodeJS" />
+		<LanguageCard source="simple-icons:nodedotjs" color="#339933" alt="nodedotjs" name="NodeJS" />
 
-		<LanguageCard source="fastify" color="fff" alt="fastify" name="Fastify" />
+		<LanguageCard source="simple-icons:fastify" color="fff" alt="fastify" name="Fastify" />
 
-		<LanguageCard source="express" color="green" alt="express" name="Express" />
+		<LanguageCard source="simple-icons:express" color="green" alt="express" name="Express" />
 
-		<LanguageCard source="prisma" color="#5163BA" alt="prisma" name="Prisma" />
+		<LanguageCard source="simple-icons:prisma" color="#5163BA" alt="prisma" name="Prisma" />
 
-		<LanguageCard source="mongodb" color="#47A248" alt="mongodb" name="MongoDB" />
+		<LanguageCard source="simple-icons:mongodb" color="#47A248" alt="mongodb" name="MongoDB" />
 	</div>
 
 	<div class="flex flex-row items-center justify-around mt-5 flex-wrap">
-		<LanguageCard source="minio" color="#C72E49" alt="minio" name="MinIO" />
+		<LanguageCard source="simple-icons:minio" color="#C72E49" alt="minio" name="MinIO" />
 
-		<LanguageCard source="apachekafka" color="fff" alt="apachekafka" name="Kafka" />
+		<LanguageCard source="simple-icons:apachekafka" color="fff" alt="apachekafka" name="Kafka" />
 
-		<LanguageCard source="ansible" color="EE0000" alt="ansible" name="Ansible" />
+		<LanguageCard source="simple-icons:ansible" color="EE0000" alt="ansible" name="Ansible" />
 
-		<LanguageCard source="kubernetes" color="#326CE5" alt="kubernetes" name="Kubernetes" />
+		<LanguageCard
+			source="simple-icons:kubernetes"
+			color="#326CE5"
+			alt="kubernetes"
+			name="Kubernetes"
+		/>
 
-		<LanguageCard source="gnubash" color="#4EAA25" alt="gnubash" name="Bash" />
+		<LanguageCard source="simple-icons:gnubash" color="#4EAA25" alt="gnubash" name="Bash" />
 	</div>
 </section>
