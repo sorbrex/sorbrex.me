eed9cd8c26ca9b17e9b26c8373e23d456dab3f7f
Nando <46705600+sorbrex@users.noreply.github.com>
2025-01-25 17:17:00 +0100

    Update About.svelte

--- src/Components/Sections/About/About.svelte
+++ src/Components/Sections/About/About.svelte
@@ -3,7 +3,7 @@
 		<h1 class="text-titleColor font-bold text-4xl pb-1">Nando</h1>
 		<h2 class="text-subtitleColor font-bold text-lg pb-1">Full Stack & iOS Developer</h2>
 		<p class="text-paragraphColor font-normal text-base">
-			I got my Master as Full Stack Developer at Start2Impact University and I'm currently a Backend Developer at Movia, in Catania.
+			I got my Master as Full Stack Developer at Start2Impact University and I'm currently a Lead Frontend Developer at Movia, in Catania.
 			My goal is to always keep up to date on the things I already know and to discover new ones.
 			I am currently studying Game Development technologies to increase my knowledge with different languages for different purposes and contexts.
 		</p>
